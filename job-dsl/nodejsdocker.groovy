89254008bea27a3d8918c4b06e26302a38b5bb28
Moody1511 <65116465+moody1511@users.noreply.github.com>
2020-11-04 21:50:55 +0100

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -14,9 +14,9 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('wardviaene/docker-nodejs-demo')
+            repositoryName('moody1511/nodejsrepo')
             tag('${GIT_REVISION,length=9}')
-            registryCredentials('dockerhub')
+            registryCredentials('dockerhubcred')
             forcePull(false)
             forceTag(false)
             createFingerprints(false)
